604f7e6075195fc275b97e92b20ff5e57a3f2384
iamwebwiz <iamwebwiz@gmail.com>
2017-02-26 02:29:11 +0100

    First commit

---
+++ home.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>TAPIT: Emergency Notification App</title>
+
+	<!-- Meta -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta name="author" content="Clevoclick Technologies">
+	<meta name="description" content="An emergency notification app">
+	<meta name="keywords" content="emergency, notification, tapit">
+	<meta name="mobile-web-app-capable" content="yes">
+	<meta name="apple-mobile-web-app-capable" content="yes">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
+
+	<!-- Custom CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/css/app.css">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap-theme.min.css">
+
+	<!-- Custom Fonts by Font-Awesome -->
+	<link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<div class="container">
+	<br>
+	<div align="center">
+		<h1 class="header">T <span><img src="assets/images/iWarning.png" width="30px" height="30px"></span> P I T</h1>
+		<p class="tagline">Your Emergency Notification App</p>
+	</div>
+	<br>
+	
+	<div class="row">
+			<div class="col-md-12 col-sm-12 col-xs-12">
+			    <div class="panel panel-default">
+			        <div class="panel-body">
+			            <h3 class="text-primary">Find Nearest Hospital <span class="fa fa-ambulance"></span></h3>
+			            <hr>
+			            <p><button type="button" class="btn btn-primary btn-md">
+			            	<span class="fa fa-map-marker"></span> Find nearest hospital
+			            </button></p>
+			            <p class="alert alert-success">Here is the nearest hospital to your area</p>
+			            <div class="img-responsive">
+			            	<img src="assets/images/lasemaResponseUnit.jpg" class="img-thumbnail">
+			            </div>
+			        </div>
+			    </div>
+			</div>
+		</div>
+	
+	
+	<div class="frsc">
+		<table width="100%">
+			<tr>
+				<td width="100px" valign="top">
+					<img src="assets/images/frsc.jpg" width="100px" height="100px">
+				</td>
+				<td width="2%"></td>
+				<td width="65%" valign="top">
+					<font size="4" color="cyan">FRSC</font>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-map-marker fa-2x"></i>
+					<p>Locate Office</p>
+				</td>
+				<td width="10%" align="center">
+					<a href="sendsos.php"><i class="fa fa-comments fa-2x"></i>
+					<p>Send SOS</p></a>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-phone fa-2x"></i>
+					<p>Call Toll Free</p>
+				</td>
+				<td width="3%">
+					<i class="fa fa-chevron-right fa-1x"></i>
+				</td>
+			</tr>
+		</table>
+	</div>
+
+	<br>
+
+	<div class="fireDept">
+		<table width="100%">
+			<tr>
+				<td width="100px" valign="top">
+					<img src="assets/images/fire-service-new-recruits-palava-21694101.jpg" width="100px" height="100px">
+				</td>
+				<td width="2%"></td>
+				<td width="65%" valign="top">
+					<font size="4" color="#FFF">THE FIRE DEPARTMENT</font>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-map-marker fa-2x"></i>
+					<p>Locate Office</p>
+				</td>
+				<td width="10%" align="center">
+					<a href="sendsos.php"><i class="fa fa-comments fa-2x"></i>
+					<p>Send SOS</p></a>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-phone fa-2x"></i>
+					<p>Call Toll Free</p>
+				</td>
+				<td width="3%">
+					<i class="fa fa-chevron-right fa-1x"></i>
+				</td>
+			</tr>
+		</table>
+	</div>
+
+	<br>
+
+	<div class="police">
+		<table width="100%">
+			<tr>
+				<td width="100px" valign="top">
+					<img src="assets/images/Nigeria-Police-Recruitment-2016.png" width="100px" height="100px">
+				</td>
+				<td width="2%"></td>
+				<td width="65%" valign="top">
+					<font size="4" color="lightgreen">POLICE</font>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-map-marker fa-2x"></i>
+					<p>Locate Office</p>
+				</td>
+				<td width="10%" align="center">
+					<a href="sendsos.php"><i class="fa fa-comments fa-2x"></i>
+					<p>Send SOS</p></a>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-phone fa-2x"></i>
+					<p>Call Toll Free</p>
+				</td>
+				<td width="3%">
+					<i class="fa fa-chevron-right fa-1x"></i>
+				</td>
+			</tr>
+		</table>
+	</div>
+
+	<br>
+
+	<div class="lastma">
+		<table width="100%">
+			<tr>
+				<td width="100px" valign="top">
+					<img src="assets/images/LASTMA.jpeg" width="100px" height="100px">
+				</td>
+				<td width="2%"></td>
+				<td width="65%" valign="top">
+					<font size="4" color="#f9f">LASTMA</font>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-map-marker fa-2x"></i>
+					<p>Locate Office</p>
+				</td>
+				<td width="10%" align="center">
+					<a href="sendsos.php"><i class="fa fa-comments fa-2x"></i>
+					<p>Send SOS</p></a>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-phone fa-2x"></i>
+					<p>Call Toll Free</p>
+				</td>
+				<td width="3%">
+					<i class="fa fa-chevron-right fa-1x"></i>
+				</td>
+			</tr>
+		</table>
+	</div>
+
+	<br>
+
+	<div class="lasema">
+		<table width="100%">
+			<tr>
+				<td width="100px" valign="top">
+					<img src="assets/images/LASEMA logo.jpg" width="100px" height="100px">
+				</td>
+				<td width="2%"></td>
+				<td width="65%" valign="top">
+					<font size="4" color="yellow">LASEMA</font>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-map-marker fa-2x"></i>
+					<p>Locate Office</p>
+				</td>
+				<td width="10%" align="center">
+					<a href="sendsos.php"><i class="fa fa-comments fa-2x"></i>
+					<p>Send SOS</p></a>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-phone fa-2x"></i>
+					<p>Call Toll Free</p>
+				</td>
+				<td width="3%">
+					<i class="fa fa-chevron-right fa-1x"></i>
+				</td>
+			</tr>
+		</table>
+	</div>
+
+	<br>
+
+	<div class="nafdac">
+		<table width="100%">
+			<tr>
+				<td width="100px" valign="top">
+					<img src="assets/images/nafdac.jpg" width="100px" height="100px">
+				</td>
+				<td width="2%"></td>
+				<td width="65%" valign="top">
+					<font size="4" color="yellow">NAFDAC</font>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-map-marker fa-2x"></i>
+					<p>Locate Office</p>
+				</td>
+				<td width="10%" align="center">
+					<a href="sendsos.php"><i class="fa fa-comments fa-2x"></i>
+					<p>Send SOS</p></a>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-phone fa-2x"></i>
+					<p>Call Toll Free</p>
+				</td>
+				<td width="3%">
+					<i class="fa fa-chevron-right fa-1x"></i>
+				</td>
+			</tr>
+		</table>
+	</div>
+
+	<br>
+
+	<div class="ndlea">
+		<table width="100%">
+			<tr>
+				<td width="100px" valign="top">
+					<img src="assets/images/60_Logo_National Drug Law Enforcement Agency (NDLEA).jpg" width="100px" height="100px">
+				</td>
+				<td width="2%"></td>
+				<td width="65%" valign="top">
+					<font size="4" color="yellow">NDLEA</font>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-map-marker fa-2x"></i>
+					<p>Locate Office</p>
+				</td>
+				<td width="10%" align="center">
+					<a href="sendsos.php"><i class="fa fa-comments fa-2x"></i>
+					<p>Send SOS</p></a>
+				</td>
+				<td width="10%" align="center">
+					<i class="fa fa-phone fa-2x"></i>
+					<p>Call Toll Free</p>
+				</td>
+				<td width="3%">
+					<i class="fa fa-chevron-right fa-1x"></i>
+				</td>
+			</tr>
+		</table>
+	</div>
+</div>
+
+<br>
+
+<!-- Script files -->
+<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="assets/js/npm.js"></script>
+</body>
+</html>
